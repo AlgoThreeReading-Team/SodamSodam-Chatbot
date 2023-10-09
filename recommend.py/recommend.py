21f8cfc8a0d478e370201b6f3eb40a8923429208
Jang-DongHo <donghoo18@naver.com>
2023-10-09 11:51:56 +0900

    [Feat] recommend.py 추가

---
+++ recommend.py/recommend.py
@@ -0,0 +1,30 @@
+# 상품 추천 모듈
+from sentence_transformers import SentenceTransformer, util
+import torch
+import pandas as pd
+
+def get_query_sim_top_k(query, model, df, top_k):
+    query_encode = model.encode(query)
+    data['hf_embeddings'] = data['hf_embeddings'].apply(eval)
+    print(df['hf_embeddings'])
+    cos_scores = util.pytorch_cos_sim(query_encode, df['hf_embeddings'])[0]
+    top_results = torch.topk(cos_scores, k=top_k)
+
+    top_indices = top_results.indices.tolist()  # 상위 상품 인덱스 리스트
+
+    # 상위 상품들의 제목과 코사인 유사도 값을 가져옴
+    top_product_info = [(df.iloc[index]['title'], cos_scores[index].item()) for index in top_indices]
+
+    return top_product_info
+
+data = pd.read_csv('recommend.py/embedded_product.csv')
+
+#data['combined_text'] = data['review_title'] + ' ' + data['review_content']
+model = SentenceTransformer('jhgan/ko-sroberta-multitask')
+
+query = "청소기"
+results = get_query_sim_top_k(query, model, data, 5)
+
+# 보기 좋게 줄바꿈하여 제목과 코사인 유사도 출력
+for idx, (title, similarity) in enumerate(results, start=1):
+    print(f"{idx}. 제목: {title}, 코사인 유사도: {similarity:.4f}")
--- server/app.py
+++ server/app.py
@@ -1,1 +1,28 @@
-# Flask 또는 FastAPI를 사용한 서버 애플리케이션
+from flask import Flask, request, jsonify, abort, render_template
+import socket
+import json
+
+# 챗봇 엔진 서버 정보
+host = "127.0.0.1"      # 챗봇 엔진 서버 IP
+port = 5050             # 챗봇 엔진 port
+
+# Flask 애플리케이션
+app = Flask(__name__)
+
+# 챗봇 엔진 query 전송 API
+@app.route('/query', methods=['POST'])
+def query():
+    body = request.get_json()
+    try:
+        # 일반 질의응답 API
+        ret = {
+            'Query' : 'test',
+        }
+        return jsonify(ret)
+
+    except Exception as ex:
+        # 오류 발생 시 500 Error
+        abort(500)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
